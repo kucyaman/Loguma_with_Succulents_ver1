2b9830d31d3088704a67d0dafdc219e2547541c1
kucyaman <joinus.ibuki@gmail.com>
2024-03-22 22:54:18 +0900

    :wrench:add cookieの設定

--- api/config/application.rb
+++ api/config/application.rb
@@ -15,5 +15,8 @@ module Api
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.i18n.default_locale = :ja
+    config.session_store :cookie_store, key: '_your_app_session'
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
   end
 end
