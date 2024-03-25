ca27a33e3ca4f3a9f5fc5d01562920100b524a18
kucyaman <joinus.ibuki@gmail.com>
2024-03-25 15:07:12 +0900

    :wrench:add 投稿用ボタンコンポーネントを作成しました

--- front/src/app/home/page.tsx
+++ front/src/app/home/page.tsx
@@ -1,12 +1,17 @@
 "use client";
-import Header from '../../components/Layouts/Header';
+import Header from '@/components/Layouts/Header';
+import CreateButton from '@/components/UI/CreateButton';
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className="container relative">
       <Header />
       {/* ここにindexページのその他のコンテンツを追加 */}
       <p>ここはログイン後のメインページです。</p>
+      <div className="absolute right-0" >
+        <CreateButton />
+      </div>
+      
     </div>
   );
 }
