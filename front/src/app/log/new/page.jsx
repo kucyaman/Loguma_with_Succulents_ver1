ae11f1b525ec047837cc984c6b2e809f1545e8a7
kucyaman <joinus.ibuki@gmail.com>
2024-04-04 17:44:24 +0900

    :lipstick:style Logフォームのスタイル修正（仮）

--- front/src/app/log/new/page.jsx
+++ front/src/app/log/new/page.jsx
@@ -14,12 +14,37 @@ export default function LogForm() {
     router.push('/home');
   };
 
-  return(
-    <form onSubmit={handleSubmit}>
-      <input type='date' name='date'/>
-      <input type='text' name='name'/>
-      <input type='text' name='note'/>
-      <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-md">投稿</button>
-    </form>
+  return (
+    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
+      <div className="relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white">
+        <div className="mt-3 text-center">
+          <h3 className="text-lg leading-6 font-medium text-gray-900">Log新規登録</h3>
+          <form className="mt-2 space-y-4" onSubmit={handleSubmit}>
+            <input
+              type="date"
+              name="date"
+              className="w-full p-2 border rounded text-black"
+            />
+            <input
+              type="text"
+              name="name"
+              placeholder="お手入れ項目"
+              className="w-full p-2 border rounded text-black"
+            />
+            <input
+              type="text"
+              name="note"
+              placeholder="お手入れメモ"
+              className="w-full p-2 border rounded text-black"
+            />
+            <button
+              type="submit"
+              className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >投稿
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 }
