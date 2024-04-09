775935e6ed926a023b862e42e8614e698b920d60
kucyaman <joinus.ibuki@gmail.com>
2024-04-09 13:01:16 +0900

    :wrench:update button,footer,image:スタイルの変更

--- front/src/app/home/page.jsx
+++ front/src/app/home/page.jsx
@@ -8,23 +8,23 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="relative">
+    <>
       <div className="fixed top-0 left-0 w-full z-10">
         <Header />
       </div>
       <div className="pt-[120px]">
         <HomeImage />
-        <Link href="/log/new" passHref>
-          <div className="absolute right-0">
+        <div className="fixed right-5 bottom-5 z-50">
+          <Link href="/log/new" passHref>
             <CreateButton />
-          </div>
-        </Link>
+          </Link>
+        </div>
         <LogIndex />
         {/* ここにindexページのその他のコンテンツを追加 */}
       </div>
       <div>
         <Footer />
       </div>
-    </div>
+    </>
   );
 }
