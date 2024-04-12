559133bc4ebbc06252043c8747c5cb3716b9cabe
kucyaman <joinus.ibuki@gmail.com>
2024-04-12 13:49:40 +0900

    :bug:refactor インデントの修正

--- front/src/app/home/components/items/logItem.jsx
+++ front/src/app/home/components/items/logItem.jsx
@@ -5,11 +5,11 @@ export default function LogItem({ log }) {
   const day = date.toLocaleDateString('ja-JP', { day: 'numeric' });
   return (
     <div className="container">
-    <div className="flex items-center text-cen bg-gray-200 p-4 my-4 mb-2 rounded-lg">
-      <p className="flex-1 text-sm md:text-base">{day}</p>
-      <p className="flex-1 text-sm md:text-base">{log.action.name}</p>
-      <p className="flex-1 text-sm md:text-base">{log.note}</p>
-    </div>
+      <div className="flex items-center text-cen bg-gray-200 p-4 my-4 mb-2 rounded-lg">
+        <p className="flex-1 text-sm md:text-base">{day}</p>
+        <p className="flex-1 text-sm md:text-base">{log.action.name}</p>
+        <p className="flex-1 text-sm md:text-base">{log.note}</p>
+      </div>
     </div>
   );
 }
